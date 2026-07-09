63864d824c1b2c45cd820ff7f4c3fc3582899b6a
AL4255 <92372109+AL4255@users.noreply.github.com>
2026-07-08 21:14:45 -0400

    Add socket and argpard imports to conn.py
    Added imports for socket and argpard libraries.

--- conn.py
+++ conn.py
@@ -1,3 +1,8 @@
 #thhis file will be for creating a tun which will be for creating "create_tun_device". this is one big part of this vpn linux connection. 
 import os
 import sys
+import socket 
+import argpard
+
+#this the magic libary for linux low level python wrapper functions
+from pytun import TunTapDevice, IFF_FUN, IFF_NO_PI
