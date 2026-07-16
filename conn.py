4fc1e6893308e83c95453b1a84381695139b09b3
AL4255 <92372109+AL4255@users.noreply.github.com>
2026-07-16 19:21:41 -0400

    Add header comment for server code in conn.py
    Added a comment header for server code.

--- conn.py
+++ conn.py
@@ -1,3 +1,6 @@
+#server code, create the tun/padding/ip
+
+
 #thhis file will be for creating a tun which will be for creating "create_tun_device". this is one big part of this vpn linux connection. 
 import os
 import sys
